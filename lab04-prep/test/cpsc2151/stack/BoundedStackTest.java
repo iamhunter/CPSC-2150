9a03b749ca5a9c89349c519b4790138d16cc7232
iams3cond <andr3whunter@gmail.com>
2016-09-14 20:14:37 -0400

    testPop dun

--- lab04-prep/test/cpsc2151/stack/BoundedStackTest.java
+++ lab04-prep/test/cpsc2151/stack/BoundedStackTest.java
@@ -42,7 +42,40 @@ public class BoundedStackTest {
 
         partial.push(new Integer(6));
         Assert.assertEquals(new BoundedStack(6, new Integer[]{1, 2, 3, 4, 5, 6}),partial);
+    }
 
+    @Test
+    public void testPop()
+    {
+        Integer returned;
+
+        //partial
+        returned = partial.pop();
+        Assert.assertEquals(new Integer(3), returned);
+        Assert.assertEquals(new BoundedStack(6, new Integer[]{1, 2}), partial);
+
+        returned = partial.pop();
+        Assert.assertEquals(new Integer(2), returned);
+        Assert.assertEquals(new BoundedStack(6, new Integer[]{1}), partial);
+
+        returned = partial.pop();
+        Assert.assertEquals(new Integer(1), returned);
+        Assert.assertEquals(new BoundedStack(6, new Integer[]{}), partial);
+
+
+
+        //full
+        returned = full.pop();
+        Assert.assertEquals(new Integer(6), returned);
+        Assert.assertEquals(new BoundedStack(6, new Integer[]{1, 2, 3, 4, 5}), full);
+
+        returned = full.pop();
+        Assert.assertEquals(new Integer(5), returned);
+        Assert.assertEquals(new BoundedStack(6, new Integer[]{1, 2, 3, 4}), full);
+
+        returned = full.pop();
+        Assert.assertEquals(new Integer(4), returned);
+        Assert.assertEquals(new BoundedStack(6, new Integer[]{1, 2, 3}), full);
     }
 
 }

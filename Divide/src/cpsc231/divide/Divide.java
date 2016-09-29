626c7604743b38c4b02350d6c9af9e70720981ab
iams3cond <andr3whunter@gmail.com>
2016-09-29 16:56:56 -0400

    Turned in Project 2 (231)

--- Divide/src/cpsc231/divide/Divide.java
+++ Divide/src/cpsc231/divide/Divide.java
@@ -1,119 +1,185 @@
 package cpsc231.divide;
 
+
+/*
+Andrew Hunter (ahunte3)
+231
+Program 2
+Due Oct 3
+Description:Divides a 16-bit binary number by an 8-bit binary number
+*/
+
+
 import java.util.Scanner;
 
 /**
- * Created by andrewmarionhunter on 9/28/16.
+ * Created by andrewhunter on 9/29/2016.
  */
 public class Divide {
-    public static void main(String[]args)
+    public static void main(String[] args)
     {
-        int decimalDividend = 875;
-        int decimalDivisor = 11;
-
+        int decimalDividend;
+        int decimalDivisor;
+        int carry = 0;
         String binaryDividend;
         String binaryDivisor;
+        int accumulator;
+        int mq;
+        String binaryAcc;
+        String binaryMq;
+        String[] arrayDividend;
+        StringBuilder accBuilder;
+        StringBuilder mqBuilder;
+        int shiftingDividend;
+        String[] tempDividendToPrint;
+        String startingBinaryDividend;
+        int decimalFinalAcc, decimalFinalMq;
 
         Scanner in = new Scanner(System.in);
         System.out.print("enter dividend: ");
-        //decimalDividend = in.nextInt();
-
+        decimalDividend = in.nextInt();
 
         System.out.print("enter divisor:");
-        //decimalDivisor = in.nextInt();
+        decimalDivisor = in.nextInt();
         System.out.println();
 
         binaryDividend = String.format("%16s", Integer.toBinaryString(decimalDividend)).replace(' ', '0');
+        startingBinaryDividend = binaryDividend;
         binaryDivisor = String.format("%8s", Integer.toBinaryString(decimalDivisor)).replace(' ', '0');
 
+
+        //BEGIN SETTING ACC AND MQ
+        arrayDividend = binaryDividend.split("");
+        accBuilder = new StringBuilder();
+        for (int i = 0; i < arrayDividend.length / 2; i++) {
+            accBuilder.append(arrayDividend[i]);
+        }
+        accumulator = Integer.parseInt(accBuilder.toString(), 2);
+
+        mqBuilder = new StringBuilder();
+        for (int i = arrayDividend.length / 2; i < arrayDividend.length; i++) {
+            mqBuilder.append(arrayDividend[i]);
+        }
+        mq = Integer.parseInt(mqBuilder.toString(), 2);
+
+        binaryAcc = String.format("%8s", Integer.toBinaryString(accumulator)).replace(' ', '0');
+        binaryMq = String.format("%8s", Integer.toBinaryString(mq)).replace(' ', '0');
+        //END SETTING ACC AND MQ
+
+        if(decimalDividend < 1 || decimalDividend > 65535 || decimalDivisor < 1 || decimalDivisor > 255 || decimalDivisor < accumulator)
+        {
+            System.out.println("Your input is invalid.");
+            return;
+        }
+
+        //PRINT BEGINNING LINES
         System.out.println("c set to 0");
         System.out.println("acc:mq set to dividend = " + decimalDividend + " decimal and " + binaryDividend + " binary");
         System.out.println("mdr set to divisor = " + decimalDivisor + " decimal and " + binaryDivisor + " binary \n");
         System.out.println("(step 0: ( MDR != 0 ) and ( MDR > ACC ) so no exceptions)");
         System.out.println("---------------------------------------------------");
+        //END PRINT BEGINNING LINES
 
-
-        for(int a = 1; a < 9; a++) {
-            String[] tempDividendToPrint = binaryDividend.split("");
-            System.out.print("step " + a + ":   0 ");
-            for (int x = 0; x < 8; x++) {
-                System.out.print(tempDividendToPrint[x]);
-            }
-            System.out.print(" ");
-            for (int x = 8; x < 16; x++) {
-                System.out.print(tempDividendToPrint[x]);
-            }
-
-            System.out.println();
+        //BEGIN LOOP
+        for(int a = 1; a <= 8; a++)
+        {
+            System.out.println("step " + a + ":   " + carry + " " + binaryAcc + " " + binaryMq);
             System.out.println("       <<              shift left");
 
-            int integerDividend = Integer.parseInt(binaryDividend, 2);
-            integerDividend = integerDividend << 1;
-            binaryDividend = String.format("%16s", Integer.toBinaryString(integerDividend)).replace(' ', '0');
-            System.out.println(binaryDividend);
+            //BEGIN SHIFTING
+            shiftingDividend = Integer.parseInt(binaryDividend, 2);
+            shiftingDividend = shiftingDividend << 1;
+            binaryDividend = String.format("%16s", Integer.toBinaryString(shiftingDividend)).replace(' ', '0');
+            //FINISH SHIFTING
 
-            tempDividendToPrint = binaryDividend.split("");
-
-            System.out.print("          0 ");
-            for (int x = 0; x < 8; x++) {
-                System.out.print(tempDividendToPrint[x]);
+            //BEGIN SETTING ACC AND MQ
+            arrayDividend = binaryDividend.split("");
+            accBuilder = new StringBuilder();
+            for (int i = 0; i < arrayDividend.length / 2; i++) {
+                accBuilder.append(arrayDividend[i]);
             }
-            System.out.print(" ");
+            accumulator = Integer.parseInt(accBuilder.toString(), 2);
+
+            mqBuilder = new StringBuilder();
+            for (int i = arrayDividend.length / 2; i < arrayDividend.length; i++) {
+                mqBuilder.append(arrayDividend[i]);
+            }
+            mq = Integer.parseInt(mqBuilder.toString(), 2);
+
+            binaryAcc = String.format("%8s", Integer.toBinaryString(accumulator)).replace(' ', '0');
+            binaryMq = String.format("%8s", Integer.toBinaryString(mq)).replace(' ', '0');
+            //END SETTING ACC AND MQ
+
+            //PRINT SHIFTED
+            tempDividendToPrint = binaryDividend.split("");
+            System.out.print("          " + carry + " " + binaryAcc + " ");
             for (int x = 8; x < 15; x++) {
                 System.out.print(tempDividendToPrint[x]);
             }
             System.out.println(".");
+            //PRINT SHIFTED
 
-            int integerDivisor = Integer.parseInt(binaryDivisor, 2);
-
-            System.out.println("        - 0 " + binaryDivisor + "         (add 1 11110101)");
+            //PRINT SUBTRACTION LINE
+            System.out.println("        - " + carry + " " + binaryDivisor + "         (add 1 11110101)");
             System.out.println("          ----------");
-            for (int x = 8; x < 16; x++) {
-                tempDividendToPrint[x] = null;
+            //END SUBTRACTED LINE
+
+            accumulator = Integer.parseInt(accBuilder.toString(), 2);
+            accumulator = accumulator - decimalDivisor;
+            binaryAcc = Integer.toBinaryString(accumulator);
+
+            binaryAcc = String.format("%8s", binaryAcc).replace(' ', '0');
+            binaryAcc = binaryAcc.substring(binaryAcc.length()-8);
+
+            if(accumulator < 0)
+            {
+                arrayDividend[15] = "0";
+
+                mqBuilder = new StringBuilder();
+                for (int i = arrayDividend.length / 2; i < arrayDividend.length; i++) {
+                    mqBuilder.append(arrayDividend[i]);
+                }
+                mq = Integer.parseInt(mqBuilder.toString(), 2);
+
+                binaryMq = String.format("%8s", Integer.toBinaryString(mq)).replace(' ', '0');
+                System.out.println("          1 " + binaryAcc + " " + binaryMq);
+                System.out.println("                            ^  set to 0 since subtract unsuccessful");
+                System.out.println("        + " + carry + " " + binaryDivisor + "           restoring add");
+
+                accumulator = accumulator + decimalDivisor;
+                binaryAcc = Integer.toBinaryString(accumulator);
+
+                binaryAcc = String.format("%8s", binaryAcc).replace(' ', '0');
+                binaryAcc = binaryAcc.substring(binaryAcc.length()-8);
+                System.out.println("          ----------");
+                System.out.println("          " + carry + " " + binaryAcc + " " + binaryMq);
+                binaryDividend = binaryAcc + binaryMq;
             }
-            StringBuilder strBuilder = new StringBuilder();
-            for (int i = 0; i < tempDividendToPrint.length / 2; i++) {
-                strBuilder.append(tempDividendToPrint[i]);
+            else
+            {
+                arrayDividend[15] = "1";
+                mqBuilder = new StringBuilder();
+                for (int i = arrayDividend.length / 2; i < arrayDividend.length; i++) {
+                    mqBuilder.append(arrayDividend[i]);
+                }
+                mq = Integer.parseInt(mqBuilder.toString(), 2);
+
+                binaryMq = String.format("%8s", Integer.toBinaryString(mq)).replace(' ', '0');
+                System.out.println("          " + carry + " " + binaryAcc + " " + binaryMq);
+                System.out.println("                            ^  set to 1 since subtract successful");
+                binaryDividend = binaryAcc + binaryMq;
             }
-
-            int accumulator = Integer.parseInt(strBuilder.toString(), 2);
-
-            int accumulatorResult = accumulator - integerDivisor;
-            String result = Integer.toBinaryString(accumulatorResult);
-            result = result.substring(result.length() - 8);
-
-            System.out.print("          " + 1 + " " + result + " ");
-
-            tempDividendToPrint = binaryDividend.split("");
-
-            for (int x = 8; x < 15; x++) {
-                System.out.print(tempDividendToPrint[x]);
-            }
-            System.out.println("0");
-
-            System.out.println("                            ^  set to 0 since subtract unsuccessful");
-            System.out.println("        + 0 " + binaryDivisor + "           restoring add");
-            System.out.println("          ----------");
-
-            accumulatorResult = accumulatorResult + integerDividend;
-            result = Integer.toBinaryString(accumulatorResult);
-            result = result.substring(result.length() - 8);
-            System.out.print("          " + 0 + " " + result + " ");
-
-            tempDividendToPrint = binaryDividend.split("");
-
-            for (int x = 8; x < 15; x++) {
-                System.out.print(tempDividendToPrint[x]);
-            }
-
-            System.out.println("\n---------------------------------------------------");
-
+            System.out.println("---------------------------------------------------");
         }
-
-
-
-
-
-
+        System.out.println("remainder     quotient");
+        System.out.println("is in ACC     is in MQ");
+        System.out.println(" " + binaryAcc + "      " + binaryMq);
+        System.out.println(" check:       binary         decimal");
+        System.out.println("         " + startingBinaryDividend + "        " + decimalDividend);
+        System.out.println("         /       " + binaryDivisor + "      /  " + decimalDivisor);
+        System.out.println("         ----------------     ------");
+        decimalFinalAcc = Integer.parseInt(binaryAcc, 2);
+        decimalFinalMq = Integer.parseInt(binaryMq, 2);
+        System.out.println("        " + binaryAcc + " " + binaryMq + "         " + decimalFinalMq + " R " + decimalFinalAcc);
     }
 }

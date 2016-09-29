520504db519c81156f202c8212ad889f11d3b485
iams3cond <andr3whunter@gmail.com>
2016-09-29 11:57:50 -0400

    More from MBP

--- Divide/src/cpsc231/divide/Divide.java
+++ Divide/src/cpsc231/divide/Divide.java
@@ -1,5 +1,6 @@
 package cpsc231.divide;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -8,19 +9,19 @@ import java.util.Scanner;
 public class Divide {
     public static void main(String[]args)
     {
-        int decimalDividend;
-        int decimalDivisor;
+        int decimalDividend = 875;
+        int decimalDivisor = 11;
 
         String binaryDividend;
         String binaryDivisor;
 
         Scanner in = new Scanner(System.in);
         System.out.print("enter dividend: ");
-        decimalDividend = in.nextInt();
+        //decimalDividend = in.nextInt();
 
 
         System.out.print("enter divisor:");
-        decimalDivisor = in.nextInt();
+        //decimalDivisor = in.nextInt();
         System.out.println();
 
         binaryDividend = String.format("%16s", Integer.toBinaryString(decimalDividend)).replace(' ', '0');
@@ -46,24 +47,49 @@ public class Divide {
         }
 
         System.out.println();
-        System.out.print("       <<              shift left");
+        System.out.println("       <<              shift left");
 
-        int integerString = Integer.parseInt(binaryDividend);
+        int integerDividend = Integer.parseInt(binaryDividend, 2);
+        integerDividend = integerDividend << 1;
+        binaryDividend = String.format("%16s", Integer.toBinaryString(integerDividend)).replace(' ', '0');
 
-    }
+        tempDividendToPrint = binaryDividend.split("");
 
-    private static void printBinary(int toBinary)
-    {
-        int remainder;
-
-        if(toBinary <=1)
+        System.out.print("          0 ");
+        for(int x = 0; x < 8; x++)
         {
-            System.out.print(toBinary);
-            return;
+            System.out.print(tempDividendToPrint[x]);
+        }
+        System.out.print(" ");
+        for(int x = 8; x < 15; x++)
+        {
+            System.out.print(tempDividendToPrint[x]);
+        }
+        System.out.println(".");
+
+        int integerDivisor =  Integer.parseInt(binaryDivisor, 2);
+
+        System.out.println("        - 0 " + binaryDivisor + "         (add 1 11110101)");
+        System.out.println("          ----------");
+        for(int x = 8; x < 16; x++)
+        {
+            tempDividendToPrint[x] = null;
+        }
+        StringBuilder strBuilder = new StringBuilder();
+        for (int i = 0; i < tempDividendToPrint.length/2; i++) {
+            strBuilder.append(tempDividendToPrint[i]);
         }
 
-        remainder = toBinary %2;
-        printBinary(toBinary >> 1);
-        System.out.print(remainder);
+        int accumulator = Integer.parseInt(strBuilder.toString(), 2);
+
+        accumulator = accumulator - integerDivisor;
+        String result = Integer.toBinaryString(accumulator);
+        result = result.substring(result.length() - 8);
+
+        System.out.println("          "+ 0 + " " + result);
+
+
+
+
     }
 }
